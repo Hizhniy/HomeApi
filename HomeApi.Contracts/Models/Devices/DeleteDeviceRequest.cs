356dbcaec6b33d96a9630caf721f9c14eb56c8d3
Hizhniy <hizhniy@gmail.com>
2023-12-18 18:56:04 +0300

    Добавьте файлы проекта.

---
+++ HomeApi.Contracts/Models/Devices/DeleteDeviceRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeApi.Contracts.Models.Devices
+{
+    /// <summary>
+    /// Удаляет устройство
+    /// </summary>
+    public class DeleteDeviceRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set;}
+    }
+}
---
+++ HomeApi.Contracts/Validation/DeleteDeviceRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentValidation;
+using HomeApi.Contracts.Models.Devices;
+
+namespace HomeApi.Contracts.Validation
+{
+    /// <summary>
+    /// Класс-валидатор запросов удаления устройства
+    /// </summary>
+    public class DeleteDeviceRequestValidator : AbstractValidator<DeleteDeviceRequest>
+    {
+        /// <summary>
+        /// Метод, конструктор, устанавливающий правила
+        /// </summary>
+        public DeleteDeviceRequestValidator() 
+        {
+            RuleFor(x => x.Id).NotEmpty();
+        }
+    }
+}
